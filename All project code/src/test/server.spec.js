8e50f6174d0ead454c40b86fc4c0a87a2639ccb8
Noam Tuchman <tuchman.bear@gmail.com>
2023-04-12 13:24:05 -0600

    unit tests from part A added

--- All project code/src/test/server.spec.js
+++ All project code/src/test/server.spec.js
@@ -25,4 +25,29 @@ describe('Server!', () => {
 
   // ===========================================================================
   // TO-DO: Part A Login unit test case
+  
+  /*  Unnote when routes and database structure.
+  it('positive : /add_user', done => {
+    chai
+      .request(server)
+      .post('/add_user')
+      .send({id: 5, name: 'John Doe', dob: '2020-02-20'})
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.message).to.equals('Success');
+        done();
+      });
+  });
+  //We are checking POST /add_user API by passing the user info in in incorrect manner (name cannot be an integer). This test case should pass and return a status 200 along with a "Invalid input" message.
+`   it('Negative : /add_user. Checking invalid name', done => {
+    chai
+        .request(server)
+        .post('/add_user')
+        .send({id: '5', name: 10, dob: '2020-02-20'})
+        .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.message).to.equals('Invalid input');
+        done();
+        });
+    });*/
 });
